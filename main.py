26bad60eb57f6a60edd61e0280950397b6febcdd
Skstud5 <skripchenko@rb.asu.ru>
2023-12-14 12:13:17 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse
+from api.users import users_router
+
+app = FastAPI(
+    description="Задание реализующее CRUD на FastAPI. Выполнил Скрипченко С.Д."
+)
+
+app.include_router(users_router, tags=["Users controller"])
+
+
+@app.get("/", response_class=PlainTextResponse, tags=["Main controller"])
+def root_page():
+    return "Добро пожаловать!"
